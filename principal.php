0e7db29f317b69864b4246c8ea8a1ba75d8aacb9
AlaxDav1d <alaxdavid13@gmai.com>
2024-04-21 13:53:00 -0300

    criando sistema de login

---
+++ principal.php
@@ -0,0 +1,17 @@
+<?php
+     if(!isset($_SESSION)){
+          session_start();
+     }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     <h1>Pagina Principal</h1>
+     <p>Bem vindo <?php echo $_SESSION['email']?></p>
+</body>
+</html>
